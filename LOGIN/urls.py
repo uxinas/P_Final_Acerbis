d2fe91393fd15417301f475b4882dadbf5fbdad1
uxinas <138539299+uxinas@users.noreply.github.com>
2023-08-04 23:26:41 -0300

    Add files via upload

--- LOGIN/urls.py
+++ LOGIN/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from LOGIN import views
+
 #from MAIN import views
 
 
@@ -9,9 +11,14 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('loginFormulario', views.loginFormulario, name='loginFormulario'),
     #path('loginFormulario_vista', views.loginFormulario_vista, name='loginFormulario_vista'),
-    path('login', views.login, name='login'),
+    #path('login', views.login, name='login'),
     path('panelCrud', views.panelCrud, name='panelcrud'),
     path('leer_usuarios', views.leerUsuarios, name='leer_usuarios'),
     path('eliminarusuarios/<usuario_nombre>', views.eliminarUsuarios, name='eliminarUsuarios'),
     path('editarusuarios/<usuario_nombre>', views.editarUsuarios, name='editarUsuarios'),
+    path('login_profile', views.login_request, name='login_profile'),
+    path('login', views.login_request, name='Login'),
+    path('register', views.register, name='Register'),
+    path('logout', LogoutView.as_view(template_name='logout.html'), name='Logout'),
+    #path('logout', LogoutView.as_view(template_name='AppCoder/logout.html'), name='Logout'),
     ]
--- LOGIN/views.py
+++ LOGIN/views.py
@@ -1,4 +1,5 @@
 #from LOGIN.models import Login
+#from django.views.generic import ListView
 from django.http import HttpResponse
 from LOGIN.forms import LoginFormulario
 from django.shortcuts import render
@@ -6,11 +7,20 @@ from LOGIN.models import Login
 from SIGNUP.models import Usuario_nuevo
 from SIGNUP.views import Usuario_nuevo_Formulario_vista
 from SIGNUP.forms import Usuario_nuevo_Formulario
+from django.contrib.auth.decorators import login_required
+from SIGNUP.models import Usuario_nuevo
+from PROFILE.forms import UserRegisterForm, UserEditForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import AuthenticationForm ,UserCreationForm 
+from django.contrib.auth.models import User
 
 
 
-def login(request):
-    return render(request,'login.html')
+#def login(request):
+#    return render(request,'login.html')
+
+
 
 
 def panelCrud(request):
@@ -37,7 +47,7 @@ def loginFormulario(request):
         miFormulario = LoginFormulario()
         return render(request, 'loginFormulario.html', {'miFormulario':miFormulario})
 
-
+@login_required
 def leerUsuarios(request):
     usuarios = Usuario_nuevo.objects.all()
     contexto = {'Usuarios':usuarios}
@@ -59,7 +69,7 @@ def editarUsuarios(request, usuario_nombre):
     usuario = Usuario_nuevo.objects.get(nombre=usuario_nombre)
     
     if request.method == "POST":
-        miFormulario = Usuario_nuevo_Formulario_vista (request.POST)
+        miFormulario = Usuario_nuevo_Formulario (request.POST)
         print(miFormulario)
 
         if miFormulario.is_valid:
@@ -75,4 +85,55 @@ def editarUsuarios(request, usuario_nombre):
     else:
         miFormulario = Usuario_nuevo_Formulario(initial={'nombre': usuario.nombre, 'email':usuario.email , 
                                                          'password':usuario.password})
-    return render(request, 'editarUsuarios.html', {'miFormulario':miFormulario, 'usuario_nombre':usuario_nombre})    
+    return render(request, 'editarUsuarios.html', {'miFormulario':miFormulario, 'usuario_nombre':usuario_nombre}) 
+
+
+
+
+def login_request(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():  # Si pasó la validación de Django
+
+            usuario = form.cleaned_data.get('username')
+            contrasenia = form.cleaned_data.get('password')
+
+            user = authenticate(username= usuario, password=contrasenia)
+
+            if user is not None:
+                login(request, user)
+
+                return render(request, "msj_usuario_logueado.html", {"mensaje":f"Bienvenido {usuario}"})
+            else:
+                return render(request, "msj_usuario_logueado.html", {"mensaje":"Datos incorrectos"})
+           
+        else:
+
+            return render(request, "msj_usuario_logueado.html", {"mensaje":"El usuario ingresado no es válido."})
+
+    form = AuthenticationForm()
+
+    return render(request, "loginc23.html", {"form": form})
+
+
+
+
+def register(request):
+
+      if request.method == 'POST':
+
+            #form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"msj_usuario_creado.html" ,  {"mensaje":"Usuario creado con exito!"})
+
+      else:
+            #form = UserCreationForm()       
+            form = UserRegisterForm()     
+
+      return render(request,"registro.html" ,  {"form":form})
